c9b9bca6f52983f71f02ade6ec959dcf5db0b3d1
Lippiece <lippiece@proton.me>
2023-02-02 14:13:26 +0200

    reuse logic from Items.tsx at Products page

--- src/routes/Products.tsx
+++ src/routes/Products.tsx
@@ -1,31 +1,12 @@
-import { Link, Outlet } from "react-router-dom";
-import products from "../data/items.json";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import ListItemButton from "@mui/material/ListItemButton";
+import { Outlet } from "react-router-dom";
+import Items from "../components/Items";
 
 const Products = () => {
   return (
     <>
       <Outlet />
       <h1>Products</h1>
-      <List>
-        {products.map((product: any) => (
-          <ListItemButton
-            component={Link}
-            to={`/odin-shopping-cart/products/${product.id}`}
-            key={product.id}
-          >
-            <ListItem>
-              <ListItemText
-                primary={product.name}
-                secondary={product.description}
-              />
-            </ListItem>
-          </ListItemButton>
-        ))}
-      </List>
+      <Items random={false} />
     </>
   );
 };
